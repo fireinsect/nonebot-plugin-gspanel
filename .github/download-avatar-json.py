58ff4c2f873804fd257e2c88bd4863a4bac1398f
fireinsect <847954981@qq.com>
2025-03-27 10:42:01 +0800

    perf:增加了avatar的整合代码

---
+++ .github/download-avatar-json.py
@@ -0,0 +1,29 @@
+import asyncio
+import json
+
+import httpx
+import requests
+
+API_URL = "https://api.github.com/repos/DGP-Studio/Snap.Metadata/contents/Genshin/CHS/Avatar"
+
+jsons=[]
+# 发送请求获取文件列表
+# response = requests.get(API_URL)
+# files = response.json()
+# re=requests.get(files[0]['download_url'])
+# for file in files:
+#     re=requests.get(file['download_url'])
+#     jsons.append(re.json())
+#     print(re.json()['Name'])
+response = httpx.get(API_URL)
+files = response.json()
+async def fetch_data():
+    async with httpx.AsyncClient() as client:
+        for file in files:
+            re=await client.get(file['download_url'])
+            jsons.append(re.json())
+            print(re.json()['Name'])
+
+asyncio.run(fetch_data())
+with open("./Avatar.json", 'w') as json_file:
+    json.dump(jsons, json_file, indent=4)
